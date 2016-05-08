405d5df8b69d4412980f21888deb5c474067e696
Yan Fan <yan.fan@ayasdi.com>
2016-05-07 20:33:17 -0700

    index and main

---
+++ main.js
@@ -0,0 +1,23 @@
+function billTotal(subtotal) {
+  // TODO: your code here
+}
+
+function animalNoise() {
+  // TODO: your code here
+}
+
+function digitalSum(n) {
+  // TODO: your code here
+}
+
+function fib(n) {
+  // TODO: your code here
+}
+
+function fizzBuzzTwo(wordA, wordB) {
+  // TODO: your code here
+}
+
+function factorial(n) {
+  // TODO: your code here
+}
